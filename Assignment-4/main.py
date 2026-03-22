eeb317da1c84c14a9e5bd5451e3bb1fc76265fdf
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-03-22 06:53:43 -0100

    Implementing Assigntment: 4 structure

--- Assignment-4/main.py
+++ Assignment-4/main.py
@@ -1,6 +1,8 @@
 import random
 
-# generate scores
+print("=== Candidate Score System ===")
+
+# generate 10 random scores
 scores = [random.randint(0, 100) for _ in range(10)]
 
 print("Original scores:", scores)
