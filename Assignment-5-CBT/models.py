8b795bf05e1fd824340d537433b6d316f0227b6f
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-03-22 10:33:45 -0100

    creating questons blueprint

---
+++ Assignment-5-CBT/models.py
@@ -0,0 +1,11 @@
+# Questions object
+class Question:
+    def __init__(self, text, options, correct_answer):
+        self.text = text
+        self.options = options
+        self.correct_answer = correct_answer
+
+    def check_answer(self, user_answer):
+        return user_answer.lower() == self.correct_answer.lower()
+# answer(stack)
+user_answer = []
