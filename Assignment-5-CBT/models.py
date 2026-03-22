831210287ead9d7bf32561af3d505b051be9cdb4
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-03-22 11:06:04 -0100

    modifying modele.py

--- Assignment-5-CBT/models.py
+++ Assignment-5-CBT/models.py
@@ -15,3 +15,26 @@ class Question:
 # answer(stack)
 user_answer = []
 
+from models import Question
+
+# Example questions
+quiz_questions = [
+    Question(
+        "What is a set in mathematics?",
+        ["A collection of ordered numbers",
+         "A collection of well-defined distinct objects",
+         "A group of random variables",
+         "A sequence of numbers"],
+        "b"
+    ),
+    Question(
+        "Which of the following is a programming language?",
+        ["HTML", "CSS", "Python", "HTTPs"],
+        "c"
+    ),
+    Question(
+        "Which data structure follows LIFO principle?",
+        ["Queue", "Tree", "Graph", "Stack"],
+        "d"
+    )
+]
