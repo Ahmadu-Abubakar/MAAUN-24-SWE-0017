17429fb4ee77583d25e13a4574d6ea0ff11f8cae
Ahmad Abubakar <ahmadabk6906@gmail.com>
2026-03-22 07:01:47 -0100

    implementing merge sort

--- Assignment-4/main.py
+++ Assignment-4/main.py
@@ -5,4 +5,37 @@ print("=== Candidate Score System ===")
 # generate 10 random scores
 scores = [random.randint(0, 100) for _ in range(10)]
 
-print("Original scores:", scores)
+# implementing merge sort
+def merge_sort(arr):
+    # base case
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_sorted = merge_sort(left_half)
+    right_sorted = merge_sort(right_half)
+
+    return merge(left_sorted, right_sorted)
+
+# merging 
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+sorted_scores = merge_sort(scores)
+print("Sorted scores:", sorted_scores)
